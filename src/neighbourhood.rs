f22a5ecf4ea5e2fa4ab1f0932dac077aee07e9ba
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-04-21 13:43:28 +0200

    fix bug in invert function, not inverting properly

--- src/neighbourhood.rs
+++ src/neighbourhood.rs
@@ -2,7 +2,7 @@ pub fn invert(route: &mut [usize]) {
     let route_len = route.len();
     let half_route_len = route_len / 2;
 
-    for i in 0..=half_route_len {
+    for i in 0..half_route_len {
         route.swap(i, route_len - i - 1);
     }
 }
