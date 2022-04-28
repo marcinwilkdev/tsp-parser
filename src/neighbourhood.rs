9d75360b4b74bc56523528961105897856707961
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-04-28 18:50:18 +0200

    add tests to neighbourhood

--- src/neighbourhood.rs
+++ src/neighbourhood.rs
@@ -10,3 +10,44 @@ pub fn invert(route: &mut [usize]) {
 pub fn swap(route: &mut [usize]) {
     route.swap(0, route.len() - 1)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn invert_even_works() {
+        let mut route = [1, 2, 3, 4, 5, 6];
+
+        invert(&mut route[1..=4]);
+
+        assert_eq!([1, 5, 4, 3, 2, 6], route);
+    }
+
+    #[test]
+    fn invert_odd_works() {
+        let mut route = [1, 2, 3, 4, 5];
+
+        invert(&mut route[1..=3]);
+
+        assert_eq!([1, 4, 3, 2, 5], route);
+    }
+
+    #[test]
+    fn swap_even_works() {
+        let mut route = [1, 2, 3, 4, 5, 6];
+
+        swap(&mut route[1..=4]);
+
+        assert_eq!([1, 5, 3, 4, 2, 6], route);
+    }
+
+    #[test]
+    fn swap_odd_works() {
+        let mut route = [1, 2, 3, 4, 5];
+
+        swap(&mut route[1..=3]);
+
+        assert_eq!([1, 4, 3, 2, 5], route);
+    }
+}
