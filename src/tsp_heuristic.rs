f9dc72a6b646d6fbe2e3b70dabcf84a19381d77d
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-06-03 18:28:32 +0200

    add something more

--- src/tsp_heuristic.rs
+++ src/tsp_heuristic.rs
@@ -7,6 +7,7 @@ mod two_opt;
 pub use krandom::KRandom;
 pub use nearest_neighbour::{NearestNeighbour, NearestNeighbourOptimized};
 pub use two_opt::TwoOpt;
+pub use two_opt::best_neighbourhood_invert;
 
 pub trait TspHeuristic {
     fn get_route(&self, tsp: &Tsp) -> Vec<usize>;
